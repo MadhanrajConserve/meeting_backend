dd33866a40e94d39cd429eeec802a5e105f87978
MadhanrajConserve <madhanraj@conservesolution.com>
2025-09-22 14:47:23 +0530

    Initial commit

---
+++ Models/DTOs/PatchMeetingDto.cs
@@ -0,0 +1,11 @@
+﻿namespace meeting_backend.Models.DTOs
+{
+    public class PatchMeetingDto
+    {
+        public string? Subject { get; set; }
+        public string? StartTime { get; set; }   
+        public string? EndTime { get; set; }   
+        public List<AttendeeDto>? Attendees { get; set; }
+        public string OrganizerEmail { get; set; } = "";
+    }
+}
